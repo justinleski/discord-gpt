b7b8780e1b7e998d8b43c3b938d1176b8206c6c3
Justin <o0justinsgamez0o@gmail.com>
2025-07-01 21:48:16 -0400

    style: Add color to UI and refactor minor issues

--- front/client-app/src/app/app.component.ts
+++ front/client-app/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { InputBarComponent } from './components/input-bar/input-bar.component';
 import { ChatLogComponent } from './components/chat-log/chat-log.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, InputBarComponent, ChatLogComponent],
+  imports: [RouterOutlet, InputBarComponent, ChatLogComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 
